566a2071dffe961ee5a1cc35542518ab407c82e9
joaorulff <jlrulff@gmail.com>
2015-06-02 09:42:44 -0300

    first commit

--- src/br/uff/ic/grad/sd/tests/Testes_main.java
+++ src/br/uff/ic/grad/sd/tests/Testes_main.java
@@ -11,17 +11,23 @@ public class Testes_main {
 		
 		ReaderWriterImpl serverImpl = new ReaderWriterImpl(1);
 		
-		/*for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 100; i++) {
 			
-			int aleatoryActions = (int)(Math.random()*3);
+			int aleatoryActions = (int)(Math.random()*2);
 			
-			Thread thread = new Thread (new ReaderWriterImpl(1));
+			if(aleatoryActions == 0){
+				serverImpl.read("othersource/arquivo1.txt", 1, 2);
+			}else{
+				serverImpl.write("othersource/arquivo1.txt", "arquivo 1 linha inserida"+ i);
+			}
 			
 			
 			
-		}*/
+			
+			
+		}
 		
-		serverImpl.read("othersource/arquivo1.txt", 1, 2);
+		
 		
 		
 		
