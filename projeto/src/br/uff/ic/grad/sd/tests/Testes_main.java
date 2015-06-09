0b1939d8f83bf13567c05d01d143fcc05f4f4bf9
paulopossas <paulopossas@id.uff.br>
2015-06-09 05:27:26 -0300

    novo teste
    testei com mais threads, parece tudo normal

--- projeto/src/br/uff/ic/grad/sd/tests/Testes_main.java
+++ projeto/src/br/uff/ic/grad/sd/tests/Testes_main.java
@@ -15,10 +15,22 @@ public class Testes_main {
 		Thread tc1 = new Thread(c1);
                 Thread tc2 = new Thread(c2);
                 Thread tc3 = new Thread(c3);
+                Cliente c4 = new Cliente();                
+		Thread tc4 = new Thread(c1);
+                Cliente c5 = new Cliente();                
+		Thread tc5 = new Thread(c1);
+                Cliente c6 = new Cliente();                
+		Thread tc6 = new Thread(c1);
+                Cliente c7 = new Cliente();                
+		Thread tc7 = new Thread(c1);
                 
 		tc1.start();                
                 tc2.start();
                 tc3.start();
+                tc4.start();
+                tc5.start();
+                tc6.start();
+                tc7.start();
                 
 		
 		
